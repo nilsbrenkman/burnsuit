224d0571a8d5e00d0b692f536fa905efcd352587
Nils Brenkman <cfbrenkman@gmail.com>
2017-04-06 23:40:41 +0200

    added leds to led manager

--- src/LedManager.cpp
+++ src/LedManager.cpp
@@ -1,15 +1,22 @@
 #include "LedManager.h"
 
-LedManager::LedManager() {
+#define CHIPSET APA102
+#define DATA_PIN A1
+#define CLOCK_PIN A0
+#define COLOR_ORDER BGR
 
+LedManager::LedManager() {
+  FastLED.addLeds<CHIPSET, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUMBER_OF_LEDS);
 }
 
-void LedManager::setLed(int i) {
-
+void LedManager::setLed(int i, CRGB color) {
+  leds[i] = color;
 }
 
 void LedManager::clearAll() {
-
+  for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
 }
 
 void LedManager::doProgramWithOffset(int program, int offset) {
--- src/LedManager.h
+++ src/LedManager.h
@@ -1,12 +1,19 @@
 #ifndef LedManager_H
 #define LedManager_H
 
+#include "Constants.h"
+#include "FastLED.h"
+
+FASTLED_USING_NAMESPACE;
+
 class LedManager {
   public:
     LedManager();
-    void setLed(int i);
+    void setLed(int i, CRGB color);
     void clearAll();
     void doProgramWithOffset(int program, int offset);
+  private:
+    CRGB leds[NUMBER_OF_LEDS];
 };
 
 #endif
